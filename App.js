0d6bb32f9197a39e6b9cb2d1748507e3545e7ef3
April Volzer <aprilvolzer@gmail.com>
2020-10-15 11:52:19 -0400

    added you are here marker (nonupdating) and default current region

--- App.js
+++ App.js
@@ -3,9 +3,14 @@ import MapView, { Marker, UrlTile, Overlay } from "react-native-maps";
 import { StyleSheet, Text, View, Dimensions } from "react-native";
 import * as Location from "expo-location";
 
+
+
+
 export default function App() {
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
+  const [latitude, setLatitude] = useState(42.930731);
+  const [longitude, setLongitude] = useState(-85.585947);
 
   useEffect(() => {
     (async () => {
@@ -16,6 +21,8 @@ export default function App() {
 
       let location = await Location.getCurrentPositionAsync({});
       setLocation(location);
+      setLatitude(location.coords.latitude); 
+      setLongitude(location.coords.longitude)
     })();
   }, []);
 
@@ -24,6 +31,7 @@ export default function App() {
     text = errorMsg;
   } else if (location) {
     text = JSON.stringify(location);
+    
   }
 
 // Map image - can replace urlTile within MapView
@@ -32,17 +40,21 @@ export default function App() {
 //  bounds={ [[42.9406,-85.5909],[42.9347,-85.5849]] }
 // />
 
+
+
   return (
     <View style={styles.container}>
       <MapView
         // region={this.state.region}
         // onRegionChange={this.onRegionChange}
         style={styles.mapStyle}
-        initialRegion={{
-          latitude: 42.930731,
-          longitude: -85.585947,
-          latitudeDelta: 0.0112,
-          longitudeDelta: 0.0111,
+        showsUerLocation={true}
+        followsUserLocation={true}
+        region={{
+          latitude: latitude,
+          longitude: longitude,
+          latitudeDelta: 0.0032,
+          longitudeDelta: 0.0031,
         }}
       >
         <UrlTile 
@@ -53,11 +65,24 @@ export default function App() {
           coordinate={{ latitude: 42.930548, longitude: -85.58581 }}
           title="Stop 1"
         ></Marker>
+        <Marker
+          coordinate={{latitude: latitude, longitude: longitude }}
+          pinColor = {'blue'}  
+          title="You are here"        
+        >
+          {/* <View style={{
+                    backgroundColor:'white',
+                    borderWidth: 1,
+                }}>
+                    <Text>You are here</Text>
+                </View> */}
+            
+        </Marker>
       </MapView>
       <View style={styles.welcome}>
         <Text style={{ fontSize: 25 }}>
           Welcome To Calvin University Campus Crawl!{" "}
-          <Text style={styles.coordinates}>{text}</Text>
+          {/* <Text style={styles.coordinates}>{text}</Text> */}
         </Text>
       </View>
     </View>
