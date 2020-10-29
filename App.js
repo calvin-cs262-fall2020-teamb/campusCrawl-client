726a9553516f3363ad2ccce1d527a0bd0f347635
Sebene <yimail08@gmail.com>
2020-10-28 20:11:48 -0400

    routing

--- App.js
+++ App.js
@@ -1,144 +1,293 @@
-import React, { useState, useEffect } from "react";
-import MapView, { Marker, UrlTile, Overlay } from "react-native-maps";
-import { StyleSheet, Text, View, Dimensions } from "react-native";
-import * as Location from "expo-location";
+import React, { Component } from "react";
+import { AppRegistry, StyleSheet, Dimensions, Image, View, StatusBar, TouchableOpacity } from "react-native";
+import { Container, Text } from "react-native";
 
-import Hideandshowcomponent, {temp} from './shared/Hideandshowcomponent'
+import MapView from 'react-native-maps';
+import Polyline from '@mapbox/polyline';
 
+class LocationA extends Component {
+  API_KEY = "AIzaSyC3KD0K9ZD67lH17t8SBJMLADejVA_CFqc";
 
-export default function App() {
-  const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
-  const [latitude, setLatitude] = useState(42.930731);
-  const [longitude, setLongitude] = useState(-85.585947);
-  const [show, setShow] = useState(true);
+  constructor(props) {
+    super(props);
 
-  const changeShow = () => {
-    setShow(false)
+    this.state = {
+      coodrinate: [
+        {
+          latitude: 42.930548,
+          longitude: -85.58581
+        },
+
+        {
+          latitude: 42.92965,
+          longitude: -85.58762
+        },
+        {
+          latitude: 42.92930,
+          longitude: -85.5845
+        }
+      ]
+    }
+
+    this.state = {
+      latitude: null,
+      longitude: null,
+      error: null,
+      concat: null,
+      coords: [],
+      x: 'false',
+      cordLatitude: 42.929232,
+      cordLongitude: -85.588334, //chapel
+    };
+
+    this.mergeLot = this.mergeLot.bind(this);
+
   }
 
-  useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestPermissionsAsync();
-      if (status !== "granted") {
-        setErrorMsg("Permission to access location was denied");
-      }
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          error: null,
+        });
+        this.mergeLot();
+      },
+      (error) => this.setState({ error: error.message }),
+      { enableHighAccuracy: false, timeout: 200000, maximumAge: 1000 },
+    );
 
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-      setLatitude(location.coords.latitude);
-      setLongitude(location.coords.longitude)
-    })();
-  }, []);
-
-  let text = "Waiting..";
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
   }
 
-  // Map image - can replace urlTile within MapView
-  // <Overlay
-  //  image={'https://calvin.edu/dotAsset/184d0710-a659-4ef6-bc18-d0ac7d9cd057/'}
-  //  bounds={ [[42.9406,-85.5909],[42.9347,-85.5849]] }
-  // />
+  mergeLot() {
+    if (this.state.latitude != null && this.state.longitude != null) {
+      let concatLot = this.state.latitude + "," + this.state.longitude
+      this.setState({
+        concat: concatLot
+      }, () => {
+        this.getDirections(concatLot, "42.930731, -85.585947,"); //bad coordinate what it od
+      });
+    }
 
-  return (
-    <View style={styles.container}>
-      <MapView
-        // region={this.state.region}
-        // onRegionChange={this.onRegionChange}
-        style={styles.mapStyle}
-        showsUerLocation={true}
-        followsUserLocation={true}
-        region={{
-          latitude: latitude,
-          longitude: longitude,
-          latitudeDelta: 0.0032,
-          longitudeDelta: 0.0031,
-        }}
-      >
-        <UrlTile
-          urlTemplate={'http://c.tile.openstreetmap.org/{z}/{x}/{y}.png'}
-        />
-        
-        {/* Possibly condense?
-        <Marker
-          coordinate={[{ latitude: 42.930548, longitude: -85.58581 },
-            { latitude: 42.92965, longitude: -85.58762 }]}
-          title={"Stop 1","Stop 2"}
-          ></Marker>*/}
+  }
 
-        <Marker
-          coordinate={{ latitude: 42.930548, longitude: -85.58581 }}
-          title="Stop 1"
-        ></Marker>
-        <Marker
+  async getDirections(startLoc, destinationLoc) {
 
-          coordinate={{ latitude: latitude, longitude: longitude }}
-          pinColor={'blue'}
-          title="You are here"
+    try {
+      let resp = await fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${startLoc}&destination=${destinationLoc}&mode=walking&key=API_KEY`)
+      let respJson = await resp.json();
+      let points = Polyline.decode(respJson.routes[0].overview_polyline.points);
+      let coords = points.map((point, index) => {
+        return {
+          latitude: point[0],
+          longitude: point[1]
+        }
+      })
+      this.setState({ coords: coords })
+      this.setState({ x: "true" })
+      return coords
+    } catch (error) {
+      console.log('masuk fungsi')
+      this.setState({ x: "error" })
+      return error
+    }
+  }
+  render() {
 
-          coordinate={{ latitude: 42.92965, longitude: -85.58762 }}
-          title="Stop 2"
-        ></Marker>
-        <Marker
-          coordinate={{ latitude: 42.92930, longitude: -85.58845 }}
-          title="Stop 3"
-        ></Marker>
-        <Marker
-          coordinate={{ latitude: 42.93095, longitude: -85.58926 }}
-          title="Stop 4"
-        ></Marker>
-        <Marker
-          coordinate={{ latitude: 42.93301, longitude: -85.58917 }}
-          title="Stop 5"
-        ></Marker>
-        <Marker
-          coordinate={{ latitude: 42.93330, longitude: -85.58635 }}
-          title="Stop 6"
-        ></Marker>
-        <Marker
-          coordinate={{ latitude: 42.93125, longitude: -85.58701 }}
-          title="Stop 7"
-        ></Marker>
-        <Marker
-          coordinate={{latitude: latitude, longitude: longitude }}
-          pinColor = {'blue'}  
-          title="You are here"        
-        >
-          {/* <View style={{
-                    backgroundColor:'white',
-                    borderWidth: 1,
-                }}>
-                    <Text>You are here</Text>
-                </View> */}
+    return (
+      <MapView style={styles.map} initialRegion={{
+        latitude: 42.930731,
+        longitude: -85.585947,
+        latitudeDelta: 1,
+        longitudeDelta: 1
+      }}>
 
-        </Marker>
+        {!!this.state.latitude && !!this.state.longitude && <MapView.Marker
+          coordinate={{ "latitude": this.state.latitude, "longitude": this.state.longitude }}
+          title={"Your Location"}
+        />}
+
+        {!!this.state.cordLatitude && !!this.state.cordLongitude && <MapView.Marker
+          coordinate={{ "latitude": this.state.cordLatitude, "longitude": this.state.cordLongitude }}
+          title={"Your Destination"}
+        />}
+
+        {!!this.state.latitude && !!this.state.longitude && this.state.x == 'true' && <MapView.Polyline
+          coordinates={this.state.coords}
+          strokeWidth={2}
+          strokeColor="red" />
+        }
+
+        {!!this.state.latitude && !!this.state.longitude && this.state.x == 'error' && <MapView.Polyline
+          coordinates={[
+            { latitude: this.state.latitude, longitude: this.state.longitude },
+            { latitude: this.state.cordLatitude, longitude: this.state.cordLongitude },
+          ]}
+          strokeWidth={2}
+          strokeColor="red" />
+        }
       </MapView>
-      {console.log(show)}
-
-      {show ? <Hideandshowcomponent changeShow={changeShow}/> : null}
-    </View>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    zIndex: 5,
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  mapStyle: {
-    zIndex: 10,
-    width: Dimensions.get("window").width,
-    height: Dimensions.get("window").height,
-  },
-  coordinates: {
-    fontSize: 10,
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
   },
 });
+
+export default LocationA;
+
+// import React, { useState, useEffect } from "react";
+// import MapView, { Marker, UrlTile, Overlay } from "react-native-maps";
+// import { StyleSheet, Text, View, Dimensions } from "react-native";
+// import * as Location from "expo-location";
+// // import MapViewDirections from 'react-native-maps-directions';
+
+// import Hideandshowcomponent, { temp } from './shared/Hideandshowcomponent'
+
+
+// export default function App() {
+//   const [location, setLocation] = useState(null);
+//   const [errorMsg, setErrorMsg] = useState(null);
+//   const [latitude, setLatitude] = useState(42.930731);
+//   const [longitude, setLongitude] = useState(-85.585947);
+//   const [show, setShow] = useState(true);
+
+//   const changeShow = () => {
+//     setShow(false)
+//   }
+
+//   useEffect(() => {
+//     (async () => {
+//       let { status } = await Location.requestPermissionsAsync();
+//       if (status !== "granted") {
+//         setErrorMsg("Permission to access location was denied");
+//       }
+
+//       let location = await Location.getCurrentPositionAsync({});
+//       setLocation(location);
+//       setLatitude(location.coords.latitude);
+//       setLongitude(location.coords.longitude)
+//     })();
+//   }, []);
+
+//   let text = "Waiting..";
+//   if (errorMsg) {
+//     text = errorMsg;
+//   } else if (location) {
+//     text = JSON.stringify(location);
+//   }
+
+//   // Map image - can replace urlTile within MapView
+//   // <Overlay
+//   //  image={'https://calvin.edu/dotAsset/184d0710-a659-4ef6-bc18-d0ac7d9cd057/'}
+//   //  bounds={ [[42.9406,-85.5909],[42.9347,-85.5849]] }
+//   // />
+
+//   return (
+//     <View style={styles.container}>
+//       <MapView
+//         // region={this.state.region}
+//         // onRegionChange={this.onRegionChange}
+//         style={styles.mapStyle}
+//         showsUerLocation={true}
+//         followsUserLocation={true}
+//         region={{
+//           latitude: latitude,
+//           longitude: longitude,
+//           latitudeDelta: 0.0032,
+//           longitudeDelta: 0.0031,
+//         }}
+//       >
+//         <UrlTile
+//           urlTemplate={'http://c.tile.openstreetmap.org/{z}/{x}/{y}.png'}
+//         />
+
+//         {/* Possibly condense?
+//         <Marker
+//           coordinate={[{ latitude: 42.930548, longitude: -85.58581 },
+//             { latitude: 42.92965, longitude: -85.58762 }]}
+//           title={"Stop 1","Stop 2"}
+//           ></Marker>*/}
+
+//         <Marker
+//           coordinate={{ latitude: 42.930548, longitude: -85.58581 }}
+//           title="Stop 1"
+//         ></Marker>
+//         <Marker
+
+//           coordinate={{ latitude: latitude, longitude: longitude }}
+//           pinColor={'blue'}
+//           title="You are here"
+
+//           coordinate={{ latitude: 42.92965, longitude: -85.58762 }}
+//           title="Stop 2"
+//         ></Marker>
+//         <Marker
+//           coordinate={{ latitude: 42.92930, longitude: -85.58845 }}
+//           title="Stop 3"
+//         ></Marker>
+//         <Marker
+//           coordinate={{ latitude: 42.93095, longitude: -85.58926 }}
+//           title="Stop 4"
+//         ></Marker>
+//         <Marker
+//           coordinate={{ latitude: 42.93301, longitude: -85.58917 }}
+//           title="Stop 5"
+//         ></Marker>
+//         <Marker
+//           coordinate={{ latitude: 42.93330, longitude: -85.58635 }}
+//           title="Stop 6"
+//         ></Marker>
+//         <Marker
+//           coordinate={{ latitude: 42.93125, longitude: -85.58701 }}
+//           title="Stop 7"
+//         ></Marker>
+//         <Marker
+//           coordinate={{ latitude: latitude, longitude: longitude }}
+//           pinColor={'blue'}
+//           title="You are here"
+//         >
+//           {/* <View style={{
+//                     backgroundColor:'white',
+//                     borderWidth: 1,
+//                 }}>
+//                     <Text>You are here</Text>
+//                 </View> */}
+
+//         </Marker>
+//       </MapView>
+//       {console.log(show)}
+
+
+
+//       {show ? <Hideandshowcomponent changeShow={changeShow} /> : null}
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     zIndex: 5,
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   mapStyle: {
+//     zIndex: 10,
+//     width: Dimensions.get("window").width,
+//     height: Dimensions.get("window").height,
+//   },
+//   coordinates: {
+//     fontSize: 10,
+//   },
+// });
 
--- shared/Hideandshowcomponent.js
+++ shared/Hideandshowcomponent.js
@@ -3,86 +3,86 @@ import { StyleSheet, Text, View, Button, Platform, TouchableOpacity } from "reac
 import { AntDesign } from '@expo/vector-icons';
 
 
-export default function Hideandshowcomponent({changeShow}) {
+export default function Hideandshowcomponent({ changeShow }) {
 
-    return (
-        <View style={styles.welcome}>
-          <Text style={{ fontSize: 25, color: '#DC0000', }}>
-            Welcome To Campus Crawl!
+  return (
+    <View style={styles.welcome}>
+      <Text style={{ fontSize: 25, color: '#DC0000', }}>
+        Welcome To Campus Crawl!
           </Text>
-          <View style={{borderBottomColor: '#A9A9A9', borderBottomWidth: 1, paddingBottom: 40,}}>  
-            <Text style={{ fontSize: 15, paddingTop: 20, fontStyle: 'italic' }}>
-              Calvin University's virtual campus tour app
+      <View style={{ borderBottomColor: '#A9A9A9', borderBottomWidth: 1, paddingBottom: 40, }}>
+        <Text style={{ fontSize: 15, paddingTop: 20, fontStyle: 'italic' }}>
+          Calvin University's virtual campus tour app
             </Text>
-          </View>
-          <View style={styles.button1}>
-            <Button title='Start a Tour' color='#000'/>
-          </View>
-          <View style={styles.button2}>
-            <Button title='Learn More' color='#fff'/>
-          </View>
-          <TouchableOpacity onPress={() => { changeShow() }}>
-            <View style={styles.close}>
-              <AntDesign name='close' size={28} color='#808080'/>
-            </View>
-          </TouchableOpacity>
+      </View>
+      <View style={styles.button1}>
+        <Button title='Start a Tour' color='#000' onPress={} />
+      </View>
+      <View style={styles.button2}>
+        <Button title='Learn More' color='#fff' />
+      </View>
+      <TouchableOpacity onPress={() => { changeShow() }}>
+        <View style={styles.close}>
+          <AntDesign name='close' size={28} color='#808080' />
         </View>
-    );
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 
 
 const styles = StyleSheet.create({
-    button1: {
-      zIndex: 20,
-      position: 'absolute',
-      left: 35,
-      bottom: 22,
-      width: 125,
-      height: 42,
-      justifyContent: 'center',
-      backgroundColor: '#FFD700',
-      borderRadius: 10,
-      shadowColor: Platform.OS === 'ios' ? '#808080': null,
-      shadowOpacity: Platform.OS === 'ios' ? 0.5: null,
-      shadowOffset: Platform.OS === 'ios' ? {width:0, height:2}: null,
-    },
-    button2: {
-      zIndex: 25,
-      position: 'absolute',
-      right: 35,
-      bottom: 22,
-      width: 125,
-      height: 42,
-      justifyContent: 'center',
-      backgroundColor: '#A9A9A9',
-      borderRadius: 10,
-      shadowColor: Platform.OS === 'ios' ? '#808080': null,
-      shadowOpacity: Platform.OS === 'ios' ? 0.5: null,
-      shadowOffset: Platform.OS === 'ios' ? {width:0, height:2}: null,
-      elevation: Platform.OS === 'android' ? 5: null,
-    },
-    welcome: {
-      zIndex: 15,
-      backgroundColor: "#fff",
-      position: "absolute",
-      paddingTop: 35,
-      alignItems: 'center',
-      bottom: 375,
-      left: 30,
-      right: 30,
-      height: 225,
-      borderWidth: 1,
-      borderColor: '#A9A9A9',
-      borderRadius: 20,
-      shadowColor: '#808080',
-      shadowOpacity: 1,
-      shadowOffset: {width:0, height:0}
-    },
-    close: {
-      zIndex: 30,
-      position: 'absolute',
-      bottom: 109,
-      left: 144,
-    }
-  });
+  button1: {
+    zIndex: 20,
+    position: 'absolute',
+    left: 35,
+    bottom: 22,
+    width: 125,
+    height: 42,
+    justifyContent: 'center',
+    backgroundColor: '#FFD700',
+    borderRadius: 10,
+    shadowColor: Platform.OS === 'ios' ? '#808080' : null,
+    shadowOpacity: Platform.OS === 'ios' ? 0.5 : null,
+    shadowOffset: Platform.OS === 'ios' ? { width: 0, height: 2 } : null,
+  },
+  button2: {
+    zIndex: 25,
+    position: 'absolute',
+    right: 35,
+    bottom: 22,
+    width: 125,
+    height: 42,
+    justifyContent: 'center',
+    backgroundColor: '#A9A9A9',
+    borderRadius: 10,
+    shadowColor: Platform.OS === 'ios' ? '#808080' : null,
+    shadowOpacity: Platform.OS === 'ios' ? 0.5 : null,
+    shadowOffset: Platform.OS === 'ios' ? { width: 0, height: 2 } : null,
+    elevation: Platform.OS === 'android' ? 5 : null,
+  },
+  welcome: {
+    zIndex: 15,
+    backgroundColor: "#fff",
+    position: "absolute",
+    paddingTop: 35,
+    alignItems: 'center',
+    bottom: 375,
+    left: 30,
+    right: 30,
+    height: 225,
+    borderWidth: 1,
+    borderColor: '#A9A9A9',
+    borderRadius: 20,
+    shadowColor: '#808080',
+    shadowOpacity: 1,
+    shadowOffset: { width: 0, height: 0 }
+  },
+  close: {
+    zIndex: 30,
+    position: 'absolute',
+    bottom: 109,
+    left: 144,
+  }
+});
