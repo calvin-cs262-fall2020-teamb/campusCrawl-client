12828611b306376b76c746eeb54b19c5ba440109
Sebene <yimail08@gmail.com>
2020-11-05 21:47:27 -0500

    learn more screen updated

--- styles/global.js
+++ styles/global.js
@@ -17,4 +17,39 @@ export const globalStyles = StyleSheet.create({
   coordinates: {
     fontSize: 10,
   },
+  modalToggle: {
+    zIndex: 15,
+    position: "absolute",
+    alignItems: 'flex-end',
+    top: 32,
+    bottom: 10,
+    left: 0,
+    right: 15,
+    height: 25,
+  },
+  learnModal: {
+    flex: 1,
+  },
+  insideLearn: {
+    flex: 1,
+    marginTop: 30,
+    marginBottom: 30,
+    marginLeft: 15,
+    marginRight: 15,
+    borderWidth: 1,
+    borderColor: '#A9A9A9',
+    borderRadius: 20,
+    shadowColor: '#808080',
+    shadowOpacity: 1,
+    shadowOffset: { width: 0, height: 0 },
+    backgroundColor: '#ffffff'
+  },
+  closeLearn: {
+    alignSelf: 'flex-end',
+    padding: 8,
+  },
+  learnContent: {
+    padding: 10,
+
+  },
 });
