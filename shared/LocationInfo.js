c9503aed13b8552fb508b1e20d23f136c0a2fa3e
ridgedejong <rad47@students.calvin.edu>
2020-11-17 07:39:12 -0500

    format welcome screen and add location info swipe

--- shared/LocationInfo.js
+++ shared/LocationInfo.js
@@ -1,87 +1,64 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View, Dimensions, Text, TouchableOpacity, Image, ScrollView, SafeAreaView } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 
 
 var width = Dimensions.get('window').width;
-    
-    
-export default function LocationInfo({locations, nextStop, endTour, id}) {
+
+
+export default function LocationInfo({ locations, nextStop, endTour, id }) {
 
     const tourStop = locations.filter(item => item.id === id)[0]
 
     return (
-        <View style={{height: 850, zIndex:30, position: 'absolute', backgroundColor: 'transparent', bottom: 0, width: width}}>
-        <ScrollView >
-            <View style={styles.clear}>
-
-            </View>
-            <View style={styles.infoContainer}>
-                <View style={styles.titlewrap}>
-                    <Text style={styles.title}>{tourStop.greeting}</Text>
+        <View style={{ height: 850, zIndex: 30, position: 'absolute', backgroundColor: 'transparent', bottom: 0, width: width,  }}>
+            <ScrollView style={{ borderTopLeftRadius: 10, borderTopRightRadius: 10 }} bounces='false'>
+                {/*Empty element in ScrollView to start infoContainer lower*/}
+                <View style={styles.clear}>
                 </View>
-                <Image style={styles.image} source={{uri: tourStop.image}}/>
-                            {/*<Image style={styles.image2} source={require('../images/background3.jpeg')}/>*/}
-                <View style={styles.infowrap}>
-                    <Text style={styles.info}>{tourStop.description}</Text>
-                </View> 
-
-
-                    {/*show Next Stop button if not last stop */}
-                    {/* buttons later
-                    {locations.filter(item => item.id === id+1)[0] ? 
-                    <TouchableOpacity style={[styles.button1, {right: 20}]} onPress={() => { nextStop(); }}>
-                        <Text style={{ fontSize: 20, color: '#3b3b3b', fontFamily: 'Lato-Regular', }}>NEXT  </Text>
-                        <AntDesign name='rightcircleo' size={22} color='#3b3b3b'/>
-                    </TouchableOpacity>
-                    : null }
-
-                <TouchableOpacity style={[styles.button2, {left: 20}]} onPress={() => { endTour(); }}>
-                    <Text style={{ fontSize: 20, color: '#3b3b3b', fontFamily: 'Lato-Regular', }}>QUIT  </Text>
-                    <AntDesign name='closecircleo' size={22} color='#3b3b3b'/>
-                    </TouchableOpacity> */}
-            </View>
+                {/*Second element in ScrollView, location info*/}
+                <View style={styles.infoContainer}>
+                    <View style={styles.titlewrap}>
+                        <Text style={styles.title}>{tourStop.greeting}</Text>
+                    </View>
+                    <Image style={styles.image} source={{ uri: tourStop.image }} />
+                    {/*<Image style={styles.image2} source={require('../images/background3.jpeg')}/>*/}
+                    <View style={styles.infowrap}>
+                        <Text style={styles.info}>{tourStop.description}</Text>
+                    </View>
+                </View>
             </ScrollView>
+
+            <View style={styles.buttonwrap}>
+                {/*show Next Stop button if not last stop */}
+                {locations.filter(item => item.id === id + 1)[0] ?
+                    <TouchableOpacity style={[styles.button1, { right: 20 }]} onPress={() => { nextStop(); }}>
+                        <Text style={{ fontSize: 20, color: '#3b3b3b', fontFamily: 'Lato-Regular', }}>NEXT  </Text>
+                        <AntDesign name='rightcircleo' size={22} color='#3b3b3b' />
+                    </TouchableOpacity>
+                    : null}
+
+                <TouchableOpacity style={[styles.button2, { left: 20 }]} onPress={() => { endTour(); }}>
+                    <Text style={{ fontSize: 20, color: '#3b3b3b', fontFamily: 'Lato-Regular', }}>QUIT  </Text>
+                    <AntDesign name='closecircleo' size={22} color='#3b3b3b' />
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     clear: {
-        height: 400,
-    },
-    Scroll: {
-        // flex: 1,
-        // backgroundColor: '#fff',
-        // zIndex: 29,
-        // bottom: 0,
-        // height: 850,
-        // width: width,
-        // position: 'absolute',
-    },
-    Container: {
-        flex: 1,
-        backgroundColor: '#800000',
-        // justifyContent: 'center',
-        // alignItems: 'center',
-        // height: 850,
-        // bottom: 0,
-        // position: 'absolute',
-        // flexGrow: 1, justifyContent: 'space-between', flexDirection: 'column'
+        height: 440,
     },
     infoContainer: {
-        // position: 'absolute',
-        // bottom: 0,
-        //height: 410,
-        // width: width,
-        // flex: 1,
-        height: 1000,
+        height: 720,
         backgroundColor: '#97252B',
         zIndex: 30,
         borderTopStartRadius: 10,
         borderTopEndRadius: 10,
         shadowColor: '#A0A0A0',
-        shadowOffset: {width:0, height:-5},
+        shadowOffset: { width: 0, height: -5 },
         shadowOpacity: 0.8,
     },
     title: {
@@ -102,13 +79,14 @@ const styles = StyleSheet.create({
     },
     infowrap: {
         position: 'absolute',
-        paddingVertical: 3,
         paddingHorizontal: 8,
+        paddingVertical: 5,
+        height: 500,
         backgroundColor: '#F2F2F2',
         zIndex: 35,
         width: 390,
         alignSelf: 'center',
-        top: 203,
+        top: 210,
         borderRadius: 8,
         shadowColor: "#000",
         shadowOpacity: 0.6,
@@ -117,14 +95,24 @@ const styles = StyleSheet.create({
     info: {
         fontFamily: 'Lato-Light',
         fontSize: 15,
-        zIndex: 40,
+        zIndex: 38,
         textAlign: 'justify',
         color: '#000'
+    },
+    buttonwrap: {
+        backgroundColor: '#3b3b3b',
+        height: 90,
+        width: width,
+        bottom: 0,
+        zIndex: 32,
+        shadowColor: "#000",
+        shadowOpacity: 0.8,
+        shadowOffset: { width: 0, height: 0 },
     },
     button1: {
         position: 'absolute',
         bottom: 24,
-        zIndex: 32,
+        zIndex: 35,
         // backgroundColor: '#FFD700',
         backgroundColor: '#E8CC16',
         paddingVertical: 12,
@@ -141,7 +129,7 @@ const styles = StyleSheet.create({
     button2: {
         position: 'absolute',
         bottom: 24,
-        zIndex: 32,
+        zIndex: 35,
         backgroundColor: '#C0C0C0',
         paddingVertical: 12,
         // paddingHorizontal: 15,
